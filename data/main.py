da4b3d11230fc1b711ce3fe426fdf5a06cf55b2a
chrultom <tchrul@o2.pl>
2016-07-18 14:56:13 +0200

    featureline update

--- data/main.py
+++ data/main.py
@@ -5,7 +5,7 @@ author = Tomasz
 
 def mainFunction():
     #x = "This is main branch and leave it alone!"
-    y = "This is brach line! It this the best!"
+    y = "This is branch line! It this the best!"
     return y
 
 
