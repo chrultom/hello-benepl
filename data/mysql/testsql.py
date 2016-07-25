3bd705bb325194bd314f0546568994be4513a75b
chrultom <tchrul@o2.pl>
2016-07-25 15:16:34 +0200

    mysql/testsql.py

--- data/mysql/testsql.py
+++ data/mysql/testsql.py
@@ -1,11 +1,33 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-conn = mysql.connector.connect(host='localhost', database='testdb', user='root', password='1python1')
+config = {
+    'user': 'root',
+    'password': '1python1',
+    'host': 'localhost',
+    'database': 'testdb'
+}
 
-if conn.is_connected():
-    print "Connected to MySQL database"
+try:
+    conn = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print "Something is wrong with your user name or password"
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print "Database does not exist"
+    else:
+        print err
+else:
+    if conn.is_connected():
+        print "Connected to MySQL database"
 
-conn.close()
+    cursor = conn.cursor()
+    cursor.execute("SELECT VERSION()")
+    data = cursor.fetchone()
 
-if not conn.is_connected():
-    print "Already disconnected"
+    print "Database version : %s " % data
+
+    conn.close()
+
+    if not conn.is_connected():
+        print "Already disconnected"
